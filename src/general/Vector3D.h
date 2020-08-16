9120ef9cb824f384fa87047bf71104f5ca36bb9e
Jonathan White <jawh@umich.edu>
2020-08-16 04:50:54 -0600

    Modified Vector3D.h     replaced tabs with spaces     replaced pass by const reference with pass by value     modified compound assignment operators and mag2 function to use binary         arithmetic operators     added function to return normalized vector

--- src/general/Vector3D.h
+++ src/general/Vector3D.h
@@ -1,35 +1,37 @@
+#pragma once
 #include <cmath>
 #include <iostream>
 
-template <class T>
+template <typename T>
 class Vector3D
 {
-	T x, y, z;
+    T x, y, z;
 public:
-	Vector3D(T xIn = 0.0f, T yIn = 0.0f, T zIn = 0.0f) : x(xIn), y(yIn), z(zIn) {}
+    Vector3D(T xIn = 0.0f, T yIn = 0.0f, T zIn = 0.0f) : x(xIn), y(yIn), z(zIn) {}
 
-	// accessors
-	auto getX() const { return x; }
-	auto getY() const { return y; }
-	auto getZ() const { return z; }
+    // accessors
+    auto getX() const { return x; }
+    auto getY() const { return y; }
+    auto getZ() const { return z; }
 
-	// binary arithmetic operators
-	auto operator * (const T& scal) const { return Vector3D<T>(x * scal, y * scal, z * scal); }
-	auto operator / (const T& scal) const { return Vector3D<T>(x / scal, y / scal, z / scal); }
-	auto operator * (const Vector3D<T>& v) { return x * v.x + y * v.y + z * v.z; }
-	auto operator + (const Vector3D<T>& v) { return Vector3D<T>(x + v.x, y + v.y, z + v.z); }
-	auto operator - (const Vector3D<T>& v) { return Vector3D<T>(x - v.x, y - v.y, z - v.z); }
+    // binary arithmetic operators
+    auto operator * (T scal) const { return Vector3D<T>(x * scal, y * scal, z * scal); }
+    auto operator / (T scal) const { return Vector3D<T>(x / scal, y / scal, z / scal); }
+    auto operator * (Vector3D<T> v) const { return x * v.x + y * v.y + z * v.z; }
+    auto operator + (Vector3D<T> v) const { return Vector3D<T>(x + v.x, y + v.y, z + v.z); }
+    auto operator - (Vector3D<T> v) const { return Vector3D<T>(x - v.x, y - v.y, z - v.z); }
 
-	// compound assignment operators
-	auto &operator *= (const T& scal) { x *= scal, y *= scal, z *= scal; return *this; }
-	auto &operator /= (const T& scal) { x /= scal, y /= scal, z /= scal; return *this; }
-	auto &operator += (const Vector3D<T>& v) { x += v.x, y += v.y, z += v.z; return *this; }
-	auto &operator -= (const Vector3D<T>& v) { x -= v.x, y -= v.y, z -= v.z; return *this; }
+    // compound assignment operators
+    auto &operator *= (T scal) { *this = *this * scal; return *this; }
+    auto &operator /= (T scal) { *this = *this / scal; return *this; }
+    auto &operator += (Vector3D<T> v) { *this = *this + v; return *this; }
+    auto &operator -= (Vector3D<T> v) { *this = *this - v; return *this; }
 
-	// special functions
-	auto mag2() const { return x * x + y * y + z * z; } // square of magnitude
-	auto mag() const { return std::sqrt(mag2()); }
+    // special functions
+    auto mag2() const { return *this * *this; } // square of magnitude
+    auto mag() const { return std::sqrt(mag2()); }
+    auto norm() const { auto _mag = mag(); return _mag ? *this / _mag : *this; }
 
-	// print function
-	void print() const {std::cout << "( " << x << " , " << y << " , " << z << " )" << std::endl; }
+    // print function
+    void print() const {std::cout << "( " << x << " , " << y << " , " << z << " )" << std::endl; }
 };
