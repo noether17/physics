08739f39fe711bb789669bc239323fd0598e9e0e
Jonathan White <jawh@umich.edu>
2020-08-22 02:40:01 -0600

    Redefined binary arithmetic operators in Vector3D.h in terms of compound     assignment operators

--- src/general/Vector3D.h
+++ src/general/Vector3D.h
@@ -7,6 +7,7 @@ class Vector3D
 {
     T x, y, z;
 public:
+    // constructor
     Vector3D(T xIn = 0.0f, T yIn = 0.0f, T zIn = 0.0f) : x(xIn), y(yIn), z(zIn) {}
 
     // accessors
@@ -14,18 +15,18 @@ public:
     auto getY() const { return y; }
     auto getZ() const { return z; }
 
-    // binary arithmetic operators
-    auto operator * (T scal) const { return Vector3D<T>(x * scal, y * scal, z * scal); }
-    auto operator / (T scal) const { return Vector3D<T>(x / scal, y / scal, z / scal); }
-    auto operator * (Vector3D<T> v) const { return x * v.x + y * v.y + z * v.z; }
-    auto operator + (Vector3D<T> v) const { return Vector3D<T>(x + v.x, y + v.y, z + v.z); }
-    auto operator - (Vector3D<T> v) const { return Vector3D<T>(x - v.x, y - v.y, z - v.z); }
-
     // compound assignment operators
-    auto &operator *= (T scal) { *this = *this * scal; return *this; }
-    auto &operator /= (T scal) { *this = *this / scal; return *this; }
-    auto &operator += (Vector3D<T> v) { *this = *this + v; return *this; }
-    auto &operator -= (Vector3D<T> v) { *this = *this - v; return *this; }
+    auto& operator *= (T scal) { x *= scal; y *= scal; z *= scal; return *this; }
+    auto& operator /= (T scal) { x /= scal; y /= scal; z /= scal; return *this; }
+    auto& operator += (Vector3D<T> v) { x += v.x; y += v.y; z += v.z; return *this; }
+    auto& operator -= (Vector3D<T> v) { x -= v.x; y -= v.y; z -= v.z; return *this; }
+
+    // binary arithmetic operators
+    auto operator * (T scal) const { Vector3D<T> tmp(*this); return *this * scal; }
+    auto operator / (T scal) const { Vector3D<T> tmp(*this); return *this / scal; }
+    auto operator * (Vector3D<T> v) const { return x * v.x + y * v.y + z * v.z; }
+    auto operator + (Vector3D<T> v) const { Vector3D<T> tmp(*this); return *this += v; }
+    auto operator - (Vector3D<T> v) const { Vector3D<T> tmp(*this); return *this -= v; }
 
     // special functions
     auto mag2() const { return *this * *this; } // square of magnitude
