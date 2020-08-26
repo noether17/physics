f0ab2dc7441b14436140b82de06895c5583d9041
Jonathan White <jawh@umich.edu>
2020-08-26 02:55:39 -0600

    Added files defining Vector struct and C and CUDA Vector functions

---
+++ src/general/Vector.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <math.h>
+#include "Vector.h"
+
+/* Print Vector in format "<v.x,v.y,v.z>\n"
+ */
+void
+v_print (Vector v)
+{
+    printf ("<%f,%f,%f>\n", v.x, v.y, v.z);
+}
+
+// //////////////////////
+// Inplace math functions
+// //////////////////////
+
+/* Replaces contents of u with *u + v.
+ */
+void
+v_add_inplace (Vector *u, Vector v)
+{
+    u->x += v.x;
+    u->y += v.y;
+    u->z += v.z;
+}
+
+/* Replaces contents of u with *u - v.
+ */
+void
+v_sub_inplace (Vector *u, Vector v)
+{
+    u->x -= v.x;
+    u->y -= v.y;
+    u->z -= v.z;
+}
+
+/* Replaces contents of u with -*u.
+ */
+void
+v_neg_inplace (Vector *u)
+{
+    u->x = -u->x;
+    u->y = -u->y;
+    u->z = -u->z;
+}
+
+/* Replaces contents of u with s*(*u).
+ */
+void
+v_scl_inplace (Coord s, Vector *u)
+{
+    u->x *= s;
+    u->y *= s;
+    u->z *= s;
+}
+
+/* Replaces contents of u with Vector in the same direction normalized
+ *     to given length.
+ */
+void
+v_norm_inplace (Coord length, Vector *v)
+{
+    v_scl_inplace (length ? length / v_mag (*v) : 0., v);
+}
+
+// ////////////////////////////////
+// Math functions returning Vectors
+// ////////////////////////////////
+
+/* Returns u + v.
+ */
+Vector
+v_add (Vector u, Vector v)
+{
+    v_add_inplace (&u, v);
+    return u;
+}
+
+/* Returns u - v.
+ */
+Vector
+v_sub (Vector u, Vector v)
+{
+    v_sub_inplace (&u, v);
+    return u;
+}
+
+/* Returns -u.
+ */
+Vector
+v_neg (Vector u)
+{
+    v_neg_inplace (&u);
+    return u;
+}
+
+/* Returns s*u.
+ */
+Vector
+v_scl (Coord s, Vector u)
+{
+    v_scl_inplace (s, &u);
+    return u;
+}
+
+/* Returns Vector in the same direction as u normalized to given
+ *     length.
+ */
+Vector
+v_norm (Coord length, Vector v)
+{
+    v_norm_inplace (length, &v);
+    return v;
+}
+
+/* Returns vector product of u and v.
+ */
+Vector
+v_cross (Vector u, Vector v)
+{
+    Coord w_x = u.y*v.z - u.z*v.y;
+    Coord w_y = u.z*v.x - u.x*v.z;
+    Coord w_z = u.x*v.y - u.y*v.x;
+    Vector w = { w_x, w_y, w_z };
+    return w;
+}
+
+// ////////////////////////////////
+// Math functions returning scalars
+// ////////////////////////////////
+
+/* Returns scalar product of u and v.
+ */
+Coord
+v_dot (Vector u, Vector v)
+{
+    Coord product = 0.;
+    product += u.x*v.x;
+    product += u.y*v.y;
+    product += u.z*v.z;
+    return product;
+}
+
+/* Returns magnitude of u.
+ */
+Coord
+v_mag (Vector u)
+{
+    return sqrt (v_dot (u, u));
+}
---
+++ src/general/cuda_Vector.cu
@@ -0,0 +1,153 @@
+#include "cuda_Vector.h"
+
+// //////////////////////
+// Inplace math functions
+// //////////////////////
+
+/* Replaces contents of u with *u + v.
+ */
+__device__
+void
+cu_v_add_inplace (Vector *u, Vector v)
+{
+    u->x += v.x;
+    u->y += v.y;
+    u->z += v.z;
+}
+
+/* Replaces contents of u with *u - v.
+ */
+__device__
+void
+cu_v_sub_inplace (Vector *u, Vector v)
+{
+    u->x -= v.x;
+    u->y -= v.y;
+    u->z -= v.z;
+}
+
+/* Replaces contents of u with -*u.
+ */
+__device__
+void
+cu_v_neg_inplace (Vector *u)
+{
+    u->x = -u->x;
+    u->y = -u->y;
+    u->z = -u->z;
+}
+
+/* Replaces contents of u with s*(*u).
+ */
+__device__
+void
+cu_v_scl_inplace (Coord s, Vector *u)
+{
+    u->x *= s;
+    u->y *= s;
+    u->z *= s;
+}
+
+/* Replaces contents of u with Vector in the same direction normalized
+ *     to given length.
+ */
+__device__
+void
+cu_v_norm_inplace (Coord length, Vector *v)
+{
+    cu_v_scl_inplace (length ? length / cu_v_mag (*v) : 0., v);
+}
+
+// ////////////////////////////////
+// Math functions returning Vectors
+// ////////////////////////////////
+
+/* Returns u + v.
+ */
+__device__
+Vector
+cu_v_add (Vector u, Vector v)
+{
+    cu_v_add_inplace (&u, v);
+    return u;
+}
+
+/* Returns u - v.
+ */
+__device__
+Vector
+cu_v_sub (Vector u, Vector v)
+{
+    cu_v_sub_inplace (&u, v);
+    return u;
+}
+
+/* Returns -u.
+ */
+__device__
+Vector
+cu_v_neg (Vector u)
+{
+    cu_v_neg_inplace (&u);
+    return u;
+}
+
+/* Returns s*u.
+ */
+__device__
+Vector
+cu_v_scl (Coord s, Vector u)
+{
+    cu_v_scl_inplace (s, &u);
+    return u;
+}
+
+/* Returns Vector in the same direction as u normalized to given
+ *     length.
+ */
+__device__
+Vector
+cu_v_norm (Coord length, Vector v)
+{
+    cu_v_norm_inplace (length, &v);
+    return v;
+}
+
+/* Returns vector product of u and v.
+ */
+__device__
+Vector
+cu_v_cross (Vector u, Vector v)
+{
+    Coord w_x = u.y*v.z - u.z*v.y;
+    Coord w_y = u.z*v.x - u.x*v.z;
+    Coord w_z = u.x*v.y - u.y*v.x;
+    Vector w = { w_x, w_y, w_z };
+    return w;
+}
+
+// ////////////////////////////////
+// Math functions returning scalars
+// ////////////////////////////////
+
+/* Returns scalar product of u and v.
+ */
+__device__
+Coord
+cu_v_dot (Vector u, Vector v)
+{
+    Coord product = 0.;
+    product += u.x*v.x;
+    product += u.y*v.y;
+    product += u.z*v.z;
+    return product;
+}
+
+/* Returns magnitude of u.
+ */
+__device__
+Coord
+cu_v_mag (Vector u)
+{
+    return sqrt (cu_v_dot (u, u));
+}
